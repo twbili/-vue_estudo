631a36ecc4626c382d5d15d9ee392ac2a008de93
ANDERSON MENEZES DO NASCIMENTO <anderson.menezes@demprel.local>
2024-11-25 16:25:01 -0300

    Estudos vue

--- src/App.vue
+++ src/App.vue
@@ -4,31 +4,39 @@
       TESTE
     </button>
   </div>
-  
-  
-  <TheHeader title="HEADER" v-show="showHeader">Teste</TheHeader>
-  <BaseAlert close="fechaAlert()" variant="success" title="ALERT" v-if="showHeader">O HEADER ESTA ATIVO</BaseAlert>
-  <BaseAlert close="fechaAlert()" variant="danger" title="ALERT" v-else>O HEADER ESTA DESATIVADO</BaseAlert>
-  
+
+  <TheHeader title="HEADER" v-show="showHeader">HEADER</TheHeader>
+  <BaseAlert :close="fechaAlert" variant="success" title="ALERT" v-if="showHeader">O HEADER ESTA ATIVO</BaseAlert>
+  <BaseAlert :close="fechaAlert" variant="danger" title="ALERT" v-else>O HEADER ESTA DESATIVADO</BaseAlert>
+
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+
+  <router-view/>
+
   <div class="center">
     <img alt="Vue logo" src="./assets/logo.png">
     <input v-model="name" type="text">
   </div>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
   
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   <h1>Lista completada</h1>
-  <div v-for ="(obj,index) in listaCompletada" :key="obj.id" class="for">
-    <img :src="obj.imgSrc" alt="https://picsum.photos/200/300?random=n">
+  <div v-for="(obj, index) in listaCompletada" :key="obj.id" class="todo-item">
+    <img :src="obj.imgSrc" :alt="'Imagem do item ' + obj.title">
     <div class="text-center">
-      {{index}}: {{ obj.title }}
+      {{ index }}: {{ obj.title }}
     </div>
   </div>
   <br>
+
   <h1>Lista não completada</h1>
-  <div v-for ="(obj,index) in listaNaoCompletada" :key="obj.id" class="for">
-    <img :src="obj.imgSrc" alt="https://picsum.photos/200/300?random=n">
+  <div v-for="(obj, index) in listaNaoCompletada" :key="obj.id" class="todo-item">
+    <img :src="obj.imgSrc" :alt="'Imagem do item ' + obj.title">
     <div class="text-center">
-      {{index}}: {{ obj.title }}
+      {{ index }}: {{ obj.title }}
     </div>
   </div>
 </template>
@@ -45,96 +53,29 @@ export default {
     TheHeader,
     BaseAlert
   },
-  data(){
-    return{
+  data() {
+    return {
       showHeader: true,
-      lista: [{
-        "userId": 1,
-        "id": 1,
-        "title": "delectus aut autem",
-        "completed": false,
-        "imgSrc": "https://picsum.photos/200/300?random=n"
-    },
-    {
-        "userId": 1,
-        "id": 2,
-        "title": "quis ut nam facilis et officia qui",
-        "completed": false,
-        "imgSrc": "https://picsum.photos/200/300?random=n"
-    },
-    {
-        "userId": 1,
-        "id": 3,
-        "title": "fugiat veniam minus",
-        "completed": false,
-        "imgSrc": "https://picsum.photos/200/300?random=n"
-    },
-    {
-        "userId": 1,
-        "id": 4,
-        "title": "et porro tempora",
-        "completed": true,
-        "imgSrc": "https://picsum.photos/200/300?random=n"
-    },
-    {
-        "userId": 1,
-        "id": 5,
-        "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-        "completed": false,
-        "imgSrc": "https://picsum.photos/200/300?random=n"
-    },
-    {
-        "userId": 1,
-        "id": 6,
-        "title": "qui ullam ratione quibusdam voluptatem quia omnis",
-        "completed": false,
-        "imgSrc": "https://picsum.photos/200/300?random=n"
-    },
-    {
-        "userId": 1,
-        "id": 7,
-        "title": "illo expedita consequatur quia in",
-        "completed": false,
-        "imgSrc": "https://picsum.photos/200/300?random=n"
-    },
-    {
-        "userId": 1,
-        "id": 8,
-        "title": "quo adipisci enim quam ut ab",
-        "completed": true,
-        "imgSrc": "https://picsum.photos/200/300?random=n"
-    },
-    {
-        "userId": 1,
-        "id": 9,
-        "title": "molestiae perspiciatis ipsa",
-        "completed": false,
-        "imgSrc": "https://picsum.photos/200/300?random=n"
-    },
-    {
-        "userId": 1,
-        "id": 10,
-        "title": "illo est ratione doloremque quia maiores aut",
-        "completed": true,
-        "imgSrc": "https://picsum.photos/200/300?random=n"
-      }]
+      lista: [ /* Sua lista de itens */ ]
     }
   },
-  methods:{
+  methods: {
     escondeHeader() {
       this.showHeader = !this.showHeader;
+    },
+    fechaAlert() {
+      // Lógica para fechar o alerta
     }
   },
-  computed:{
-    listaCompletada(){
+  computed: {
+    listaCompletada() {
       return this.lista.filter(lista => lista.completed);
     },
-    listaNaoCompletada(){
+    listaNaoCompletada() {
       return this.lista.filter(lista => !lista.completed);
     }
   }
 }
-
 </script>
 
 <style>
@@ -146,7 +87,9 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-.for{font-family: Avenir, Helvetica, Arial, sans-serif;
+
+.todo-item {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -159,6 +102,19 @@ export default {
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  text-align: center; /* Caso queira centralizar também o texto dentro do input */
+  text-align: center; 
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
